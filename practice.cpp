72cde150747325ad18c5b2d0f737745570f3a147
ifaiq19 <faiq.019@gmail.com>
2020-06-21 14:54:02 +0500

    Working w/ numbers

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
@@ -64,8 +65,31 @@ int main()
 ***/
 
 //Working with numbers
-
+/***
 int main()
 {
-    
+    int number;
+    double num1, num2;
+    number = 10;
+    cout << number << endl;
+    number++;
+    cout << number << endl;
+    number--;
+    cout << number << endl;
+    number+=100;
+    cout << number << endl;
+    number-=140;
+    cout << number << endl;
+    num1=10;
+    num2=5.5;
+    cout << num1/num2 << endl;
+    cout << pow(10, 2) << endl;
+    cout << sqrt(25) << endl;
+    cout << round(88.6) << endl;
+    cout << ceil(55.1) << endl;
+    cout << floor(99.9) << endl;
+    cout << fmax(10,11) << endl;
+    cout << fmin(4,110) << endl;
+    return 0;
 }
+***/
