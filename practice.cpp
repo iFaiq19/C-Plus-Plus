8776a299fbbe02b252299b0e517d9f3c759e5ce3
ifaiq19 <faiq.019@gmail.com>
2020-06-21 14:10:19 +0500

    Starting out

--- practice.cpp
+++ practice.cpp
@@ -28,16 +28,20 @@ int main()
 *///
 
 //Data Types
-/***
+
 int main()
 {
     char grade = 'A';
     string name = "Faiq";
     int per_round = 79;
     float marks = 175.25;
+    double nothing = 56.6666;
+    bool cleared = true;
     cout << "Student: " << name << endl;
     cout << "Grade: " << grade << endl;
     cout << "Percentage: " << per_round << endl;
     cout << "Marks: " << marks << endl;  
+    cout << "Double can store more decimal points than float " << nothing << endl;
+    cout << "Passed the exam? (1 for yes, 0 for no): " << cleared;
+    return 0
 }
-*///
