03d670020dbe3f8923b9c0caaca7c3f156b4d203
ifaiq19 <faiq.019@gmail.com>
2020-06-21 16:53:17 +0500

    Powers of a number

---
+++ powers.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void powers(int num);
+
+int main()
+{
+    int num;
+    cout << "Enter your number: ";
+    cin >> num;
+
+    powers(num);
+}
+
+//-----------------------------------
+
+void powers(int num)
+{
+    int sq, cube, fourth;
+    sq = pow(num, 2);
+    cube = pow(num, 3);
+    fourth = pow(num, 4);
+
+    cout << "The square of your number is: " << sq << endl
+         << "The cube of your number is: " << cube << endl
+         << "The fourth power of your number is: " << fourth << endl;
+}
