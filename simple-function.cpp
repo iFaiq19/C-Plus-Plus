c811e7432d2e1cf70be49260e8e6d619e1480bb3
ifaiq19 <faiq.019@gmail.com>
2020-06-21 16:22:35 +0500

    Functions-arguments for functions

--- simple-function.cpp
+++ simple-function.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void starline();            //function declaration (prototype)
+void starline(int n);            //function declaration (prototype)
 
 int main()
 {
-    starline();         //call to function
+    starline(60);         //call to function
     cout << "Data type Range" << endl;
-    starline();         //call to function
+    starline(60);         //call to function
     cout << "char;  -128 to 127" << endl
          << "short; -32,768 to 32,767" << endl
-         << "int;   system dependent" << endl
+         << "int;    system dependent" << endl
          << "long;  -2,147,483,648 to 2,147,48,647" << endl;
-    starline();         //call to function
+    starline(60);         //call to function
     return 0;
 }
 //---------------------------------------------------------
 //function definition
-void starline()         //function declarator
+void starline(int n)         //function declarator
 {
-    for (int j=0; j<45; j++)            //function body
+    for (int j=0; j<n; j++)            //function body
         cout << "*";
     cout << endl;
 }
