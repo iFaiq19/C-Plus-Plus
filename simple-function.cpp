32716cd92c54606b9eafa6df66f6b78cf67858d1
ifaiq19 <faiq.019@gmail.com>
2020-06-21 16:40:43 +0500

    Practice

--- simple-function.cpp
+++ simple-function.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
 using namespace std;
 
-void starline(int n);            //function declaration (prototype)
+void starline(char ch, int n);            //function declaration (prototype)
 
 int main()
 {
-    cout << "Enter number: ";
-    int n;
-    cin >> n;
-    starline(n);         //call to function
+    starline('-', 60);         //call to function
     cout << "Data type Range" << endl;
-    starline(n);         //call to function
+    starline('=', 60);         //call to function
     cout << "char;  -128 to 127" << endl
          << "short; -32,768 to 32,767" << endl
          << "int;    system dependent" << endl
          << "long;  -2,147,483,648 to 2,147,48,647" << endl;
-    starline(n);         //call to function
+    starline('+', 60);         //call to function
     return 0;
 }
 //---------------------------------------------------------
 //function definition
-void starline(int n)         //function declarator
+void starline(char ch, int n)         //function declarator
 {
     for (int j=0; j<n; j++)            //function body
-        cout << "*";
+        cout << ch;
     cout << endl;
 }
