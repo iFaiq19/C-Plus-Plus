321ba4bac3dd622883461d6396200c23c1727520
ifaiq19 <faiq.019@gmail.com>
2020-06-21 16:34:39 +0500

    Practice

--- simple-function.cpp
+++ simple-function.cpp
@@ -5,14 +5,17 @@ void starline(int n);            //function declaration (prototype)
 
 int main()
 {
-    starline(60);         //call to function
+    cout << "Enter number: ";
+    int n;
+    cin >> n;
+    starline(n);         //call to function
     cout << "Data type Range" << endl;
-    starline(60);         //call to function
+    starline(n);         //call to function
     cout << "char;  -128 to 127" << endl
          << "short; -32,768 to 32,767" << endl
          << "int;    system dependent" << endl
          << "long;  -2,147,483,648 to 2,147,48,647" << endl;
-    starline(60);         //call to function
+    starline(n);         //call to function
     return 0;
 }
 //---------------------------------------------------------
