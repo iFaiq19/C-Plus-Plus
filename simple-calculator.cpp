93ee6bf8172bba5982e06433b269e101e38919f6
ifaiq19 <faiq.019@gmail.com>
2020-06-25 01:32:46 +0500

    calculator 2.0

--- simple-calculator.cpp
+++ simple-calculator.cpp
@@ -2,37 +2,6 @@
 #include <sstream>
 using namespace std;
 
-/*
-void calculator(double num1, double num2, char op);
-
-int main()
-{
-    char op;
-    double num1, num2, total;
-    cout << "Enter 1st number: ";
-    cin >> num1;
-    cout << "Enter one operator (+, -, *, /): ";
-    cin >> op;
-    cout << "Enter 2nd number: ";
-    cin >> num2;
-    calculator(num1, num2, op);
-}
-
-void calculator(double num1, double num2, char op)
-{
-    int ans;
-    ans = num1;
-    if (op == '+')
-    ans += num2;
-    else if (op == '-')
-    ans -= num2;
-    else if (op == '*')
-    ans *= num2;
-    else if (op == '/')
-    ans /= num2;
-    cout << "Answer is: " << ans;
-}
-*/
 // calculator 2.0 ("Aaqib")
 
 double addNumbers(double, double);
@@ -43,7 +12,7 @@ double multNumbers(double, double);
 int main(){
     double result = 0;
     int operands;
-    cout << "Enter number of Operatands: ";
+    cout << "Enter number of Operands: ";
     cin >> operands;
     cout << endl;
     char symbol = 'z';
