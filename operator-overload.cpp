4d9a5319b14946fadf22e9fb5070234491f27707
ifaiq19 <faiq.019@gmail.com>
2020-07-05 15:56:22 +0500

    operator overloading

--- operator-overload.cpp
+++ operator-overload.cpp
@@ -11,26 +11,59 @@ class Operator
         void get_count()
         {cout << count;}
 
-        void operator ++ (int n)  //eg c1++
-        {count +=1 ;}
+        Operator operator ++ (int n)  //eg c1++
+        {count +=1 ;
+        Operator temp;
+        temp.count = count;
+        return temp;
+        }
 
-        void operator -- (int n)  //eg c1--
-        {count -=1 ;}
+        Operator operator -- (int n)  //eg c1--
+        {count -=1 ;
+        Operator temp;
+        temp.count = count;
+        return temp;
+        }
 
-        void operator += (int n)  //eg c1 += 25
-        {count +=n ;}
+        Operator operator += (int n)  //eg c1 += 25
+        {count +=n ;
+        Operator temp;
+        temp.count = count;
+        return temp;
+        }
 
-        void operator -= (int n)  //eg c1 -= 25
-        {count -= n ;}
+        Operator operator -= (int n)  //eg c1 -= 25
+        {count -= n ;
+        Operator temp;
+        temp.count = count;
+        return temp;
+        }
+
+        Operator operator + (int n)  //eg c1 - 25
+        {count += n ;
+        Operator temp;
+        temp.count = count;
+        return temp;
+        }
+
+        Operator operator - (int n)  //eg c1 - 25
+        {count -= n ;
+        Operator temp;
+        temp.count = count;
+        return temp;
+        }
 };
 
 int main()
 {
-    Operator c1;
+    Operator c1, c2;
     cout << "\nC1:\t"; c1.get_count();  // 0
+    cout << "\nC2:\t"; c2.get_count();  // 0
     c1 ++; // 1
     c1 += 100;  // 101
     c1 -= 2;  // 99
     c1 --;  // 98
+    c2 = c1 + 300;  // 398
     cout << "\nC1:\t"; c1.get_count();  // 98
+    cout << "\nC2:\t"; c2.get_count();  // 398
 }
