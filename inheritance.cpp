1ef06a1e142b1cd9e75f5cbf3af4dbfbde3239db
Faiq <faiq.019@gmail.com>
2020-07-07 19:58:24 +0500

    learning inheritance

--- inheritance.cpp
+++ inheritance.cpp
@@ -2,46 +2,46 @@
 #include <conio.h>
 using namespace std;
 
-class Count
+class counter
 {
     protected:
         unsigned int count;
     public:
-        Count() : count(0)
+        counter() : count(0)
         {   }
 
-        Count(int n) : count(n)
+        counter(int n) : count(n)
         {   }
 
         unsigned int get_count() const
         {return count;}
 
-        Count operator ++ (int n)
-        {return Count(count += 1);}
+        counter operator ++ (int n)
+        {return counter(count += 1);}
                 
-        Count operator += (int n)
-        {return Count(count += n);}
+        counter operator += (int n)
+        {return counter(count += n);}
 };
 
-class CountDec : public Count
+class countDec : public counter
 {
     public:
-        CountDec() : Count()
+        countDec() : counter()
         {   }
 
-        CountDec(int n) : Count(n)
+        countDec(int n) : counter(n)
         {   }
 
-        Count operator -- (int n)
-        {return Count(count -= 1);}
+        counter operator -- (int n)
+        {return counter(count -= 1);}
 
-        Count operator -= (int n)
-        {return Count(count -= n);}
+        counter operator -= (int n)
+        {return counter(count -= n);}
 };
 
 int main()
 {
-    CountDec number1, number2(66);
+    countDec number1, number2(66);
     cout << "Number 1: " << number1.get_count() << endl;
     cout << "Number 2: " << number2.get_count() << endl;
 
@@ -54,7 +54,7 @@ int main()
     number2++;number2+=5;
     cout << "Number 2 (after increment): " << number2.get_count() << endl;
 
-    CountDec number3 = number2;
+    countDec number3 = number2;
     cout << "Number 3: " << number2.get_count() << endl;
 
     getch();
