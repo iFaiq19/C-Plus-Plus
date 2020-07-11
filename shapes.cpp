19a63cb6b80b501a5d226c61ab4426da4cd6ef87
Faiq <faiq.019@gmail.com>
2020-07-11 15:34:40 +0500

    working

---
+++ shapes.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class shape
+{
+    protected:
+        double length, width;
+    public:
+        void get_data()
+        {
+            cout << "Enter length/base: "; cin >> length;
+            cout << "Enter height/width: "; cin >> width;
+        }
+
+        virtual void display_area() = 0;
+};
+
+class triangle : public shape
+{
+    public: 
+        void display_area()
+        {
+            cout << "The area of triangle with height= " << width << " and base= " << length << " is= "
+                 << (length*width)/2 << endl;
+        }
+};
+
+class rectangle : public shape
+{
+    public: 
+        void display_area()
+        {
+            cout << "The area of rectangle with width= " << width << " and length= " << length << " is= "
+                 << length*width << endl;
+        }
+};
+
+int main()
+{
+    triangle t1;
+    rectangle r1;
+    shape* arr[2];
+    arr[0] = &t1;
+    arr[1] = &r1;
+
+    arr[0]->get_data();
+    arr[0]->display_area();
+
+    arr[1]->get_data();
+    arr[1]->display_area();
+
+    return 0;
+}
